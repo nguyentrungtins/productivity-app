aad4bacfa8344c7e50b51f0121032fe7e722b3b7
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-04 20:47:32 +0700

    remove Header to Post request

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,9 +15,7 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + "login", userData, {
-    headers: { "content-type": "application/x-www-form-urlencoded" },
-  });
+  const response = await axios.post(API_URL + "login", userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
