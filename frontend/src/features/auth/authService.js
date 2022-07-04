2b48496cd3ecff961f91212e111281b647c74ac9
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-04 20:40:06 +0700

    add Header to Post request

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,38 +1,46 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = '/api/users/'
+const API_URL = "/api/users/";
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
 
-  return response.data
-}
+  return response.data;
+};
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post(API_URL + "login", userData, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Accept: "*/*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+    },
+  });
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
 
-  return response.data
-}
+  return response.data;
+};
 
 // Logout user
 const logout = () => {
-  localStorage.removeItem('user')
-}
+  localStorage.removeItem("user");
+};
 
 const authService = {
   register,
   logout,
   login,
-}
+};
 
-export default authService
+export default authService;
