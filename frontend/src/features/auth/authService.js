81ff1eb4b08da0004571c8a392fad04df3d7dbab
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-04 20:44:11 +0700

    add Header to Post request

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,7 +15,9 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + "login", userData);
+  const response = await axios.post(API_URL + "login", userData, {
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+  });
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
