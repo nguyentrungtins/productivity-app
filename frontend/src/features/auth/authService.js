00af8951836abcc084ca57bbcbef5f987026836d
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-04 20:42:27 +0700

    remove Header to Post request

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,15 +15,7 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + "login", userData, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      Accept: "*/*",
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-    },
-  });
+  const response = await axios.post(API_URL + "login", userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
